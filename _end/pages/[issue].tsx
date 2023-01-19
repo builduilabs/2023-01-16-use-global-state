df7553f0323687eea221e8733a0ab0e998fe9143
Sam Selikoff <sam.selikoff@gmail.com>
2023-01-19 00:08:36 -0500

    wip

--- _end/pages/[issue].tsx
+++ _end/pages/[issue].tsx
@@ -1,8 +1,7 @@
+import NewComment from "@/components/new-comment";
 import Image from "next/image";
 import { useState } from "react";
 import Tony from "../public/tony.jpeg";
-import Sam from "../public/sam.jpeg";
-import { createGlobalState } from "react-hooks-global-state";
 
 export default function Issue({ issue }: any) {
   let [isShowingNewComment, setIsShowingNewComment] = useState(false);
@@ -53,7 +52,7 @@ export default function Issue({ issue }: any) {
         <div className="mt-6 text-right">
           <button
             onClick={() => setIsShowingNewComment(true)}
-            className="rounded bg-green-500 px-4 py-1 text-sm font-semibold text-white hover:bg-green-500/90"
+            className="rounded border border-gray-300 bg-gray-100 px-4 py-1 text-sm font-semibold text-gray-600 hover:bg-gray-200"
           >
             Add comment
           </button>
@@ -63,79 +62,7 @@ export default function Issue({ issue }: any) {
   );
 }
 
-let { useGlobalState } = createGlobalState<{
-  draftComments: Record<string, string>;
-}>({
-  draftComments: {},
-});
-
-function NewComment({
-  issueId,
-  onCancel = () => {},
-}: {
-  issueId: string;
-  onCancel: () => void;
-}) {
-  let [draftComments, setDraftComments] = useGlobalState("draftComments");
-
-  return (
-    <div>
-      <div className="mt-2 flex">
-        <Image
-          src={Sam}
-          className="mr-2 h-11 w-11 rounded-full border"
-          alt=""
-        />
-        <div className="w-full overflow-hidden rounded border border-gray-300">
-          <div className="flex border-b border-gray-300 bg-gray-50 px-2 py-5">
-            <span className="-mb-[21px] -mt-3 flex items-center rounded-t border-x border-t border-gray-300 bg-white px-3 text-sm text-gray-700">
-              Write
-            </span>
-          </div>
-          <div className="space-y-4 p-2 text-sm">
-            <textarea
-              className="w-full rounded border border-gray-300 bg-gray-50 p-3"
-              placeholder="Leave a comment"
-              value={draftComments[issueId]}
-              onChange={(e) =>
-                setDraftComments({
-                  ...draftComments,
-                  [issueId]: e.target.value,
-                })
-              }
-              name=""
-              id=""
-              rows={6}
-            ></textarea>
-          </div>
-        </div>
-      </div>
-      <div className="mt-4 flex justify-end space-x-3">
-        <button
-          onClick={onCancel}
-          className="rounded border border-gray-300 bg-gray-100 px-4 py-1 text-sm font-semibold text-gray-600 hover:bg-gray-200"
-        >
-          Cancel
-        </button>
-        <button
-          onClick={() => alert(draftComments)}
-          className="rounded border border-green-600 bg-green-500 px-4 py-1 text-sm font-semibold text-white hover:bg-green-600"
-        >
-          Comment
-        </button>
-      </div>
-    </div>
-  );
-}
-
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-}
-
-export async function getStaticProps(context: any) {
+export async function getServerSideProps(context: any) {
   return {
     props: { issue: issues.find((i) => i.id === context.params.issue) },
   };
